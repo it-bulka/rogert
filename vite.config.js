9c3d031cff68e1963202ead594954367a07d3db8
IT.bulka <i.it.bulka@gmail.com>
2026-02-09 18:09:49 +0100

    change base in config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   build: {
-    base: '/rogert/',
+    base: '.',
     outDir: 'dist',
     rollupOptions: {
       input: {
